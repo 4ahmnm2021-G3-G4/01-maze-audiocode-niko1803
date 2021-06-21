dfd2615ec15d98f06f9aeb62467012442363d514
Niko1803 <nicolas.korb03@htl-salzburg.ac.at>
2021-06-21 09:10:58 +0200

    Update

---
+++ Assets/MyGame/Scripts/TriggerBasket.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBasket : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "needle")
+        {
+            Debug.Log("NeedleTriggered");
+        }
+    }
+}
